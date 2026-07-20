92923fa9d4075794cc5d877adff7db76dbe8b4b1
fakhrin31 <fakhrindev@gmail.com>
2026-07-20 10:29:12 +0700

    feat: add Docker support with multi-stage build, Nginx proxy, and standalone output configuration

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "standalone",
 };
 
 const withAnalyzer = withBundleAnalyzer({
